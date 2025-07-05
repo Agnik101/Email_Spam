0034d2326820c64f47b11da97695a6f7e7f1c00f
Agnik Gupta <agnikgupta4@gmail.com>
2025-07-05 18:56:06 +0530

    Update my_email.py

--- my_email.py
+++ my_email.py
@@ -60,8 +60,8 @@ SPAM_WORDS = [
     r'(?i)\bmeet\s*girls\b', r'(?i)\bcasino\b', r'(?i)\bcredit\b',
     r'(?i)\bloan\b', r'(?i)\bmoney\b', r'(?i)\badult\s*dating\b',
     r'(?i)\bsexy\b', r'(?i)\bhot\s*singles\b', r'(?i)\bhot\b',
-    r'(?i)\rape\b', r'(?i)\blottery\b',r'(?i)\free\b' , 
-    r'(?i)\subscribe\b' ,r'(?i)\rape\b'
+    r'(?i)\rape\b' , r'(?i)\blottery\b', r'(?i)\free\b' , 
+    r'(?i)\subscribe\b' , r'(?i)\rape\b'
 ]
 
 def detect_spam_words(text):
