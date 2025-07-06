5d0ed79fb6958fff179a395008144c277509ac49
Agnik Gupta <agnikgupta4@gmail.com>
2025-07-06 23:46:36 +0530

    Update my_email.py

--- my_email.py
+++ my_email.py
@@ -14,7 +14,7 @@ st.markdown("""
 
 /* Style for the sidebar with increased width */
 [data-testid="stSidebar"] {
-    background-color: #477A17;
+     background: linear-gradient(to bottom, #650021, #8a2b62);
     padding: 0.5rem;
     color: white;
     width: 250px;  /* Increase the sidebar width */
